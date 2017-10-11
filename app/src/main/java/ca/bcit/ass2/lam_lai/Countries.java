40d673231605eb2594865c4af9c8672ea3e8e6bc
angusl2004 <angusl2004@gmail.com>
2017-10-11 12:07:40 -0700

    init add, has 2 list activities and a detail activity implemented

---
+++ app/src/main/java/ca/bcit/ass2/lam_lai/Countries.java
@@ -0,0 +1,34 @@
+package ca.bcit.ass2.lam_lai;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Countries extends ListActivity {
+    String[] countries = new String[]{"Canada"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Create the adapter that will allow listview to work
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, countries);
+
+        //Set the adapter to the listview object
+        setListAdapter(adapter);
+    }
+
+    public void onListItemClick(ListView list, final View view,
+                                int position, long id) {
+        Intent intent = new Intent(Countries.this, Details.class);
+
+        intent.putExtra("continent", countries[position]);
+
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/ca/bcit/ass2/lam_lai/MainActivity.java
@@ -0,0 +1,36 @@
+package ca.bcit.ass2.lam_lai;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+    String[] continents = new String[]{"North America"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Create the adapter that will allow listview to work
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, continents);
+
+        //Set the adapter to the listview object
+        setListAdapter(adapter);
+    }
+
+    public void onListItemClick(ListView list, final View view,
+                                int position, long id) {
+        Intent intent = new Intent(MainActivity.this, Countries.class);
+
+        intent.putExtra("continent", continents[position]);
+
+        startActivity(intent);
+    }
+}
+
